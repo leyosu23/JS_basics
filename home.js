b0baaaa246b012b286412f298edb0c834a2ace01
leyosu23 <leyosu23@gmail.com>
2021-06-11 21:38:44 -0400

    Initial Commit

---
+++ home.js
@@ -0,0 +1,80 @@
+//console.log('hello');
+//alert('yo')
+
+
+// Variables
+var b ='hello';
+
+//var age = prompt('What is your age?');
+
+//document.getElementById('someText').innerHTML = age;
+
+
+// Functions
+
+function fun() {
+    var name = prompt("What's your name");
+    var result = 'Hello' + name;
+    console.log(result);
+}
+
+//fun();
+
+
+/*
+// while loops
+
+var num = 0;
+while(num <100){
+    num +=1;
+    console.log(num);
+}
+
+// for loop
+for (let num = 0; num <100; num++){
+    console.log(num)
+}
+*/
+
+
+/*
+// String in Javascript
+let fruits = 'banana\napple';
+
+console.log(fruits.indexOf('app'));
+console.log(fruits.slice(2,6));
+console.log(fruits.replace('ban',123));
+console.log(fruits.toUpperCase());
+console.log(fruits[2]);
+console.log(fruits.split('')); // split by characters
+*/
+
+/*
+let fruits = ['banana', 'apple', 'orange'];
+fruits = new Array('banana', 'apple', 'orange');
+
+console.log(fruits[2]);
+fruits[0] = 'pear';
+console.log(fruits);
+
+console.log(fruits.join('-'));
+console.log(fruits.pop(), fruits); //poped off the last index item
+console.log(fruits.push('blackberry'), fruits); 
+fruits.shift();
+fruits.unshift('kiwi');
+
+let numbers = [5,10,20,60,1];
+console.log(numbers.sort(function(a,b) {return a-b})); //sorting ascending order
+*/
+
+/*
+// Dictionaries
+let student = {
+    first: 'John',
+    last: 'Doe',
+    age:23
+};
+
+console.log(student.first);
+*/
+
